ff34e925439e54bacdca84f6169abf83043f7683
Aurelien Lourot <software@lourot.dev>
2023-07-02 17:48:58 +0200

    Increase distance between homepage features on mobile

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -46,7 +46,7 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--4 homepage-feature')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
